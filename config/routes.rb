434ba959f32d9c5ec44583f1e9aaeb304b4bec0b
Ambreen shafiq <ambren.shafiq@gmail.com>
2024-02-22 10:52:10 +0400

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "up" => "rails/health#show", as: :rails_health_check
-
-
+ get 'about' , to: 'pages#about'
+ # get "up" => "rails/health#show", as: :rails_health_check
 end
+
+
